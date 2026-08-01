94a0c59b3f2966571b9ccdd6ab10f983f188e403
Sharath kumar B <sharx2503@gmail.com>
2026-08-01 15:43:55 +0530

    Time: 0 ms (100%), Space: 44.7 MB (37.93%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -3,7 +3,7 @@ class Solution {
         int s=0;
         for(int i=0;i<accounts.length;i++){
             int temp=0;
-           for(int j=0;j<accounts[0].length;j++){
+           for(int j=0;j<accounts[i].length;j++){
             temp+=accounts[i][j];
            }
            if(s<temp){
